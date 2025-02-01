be5e004dc62fa35072f4c3721bf260ff5db7ddf3
mrvigneshvt <mrvigneshvt@gmail.com>
2025-02-01 17:35:07 +0530

    server init

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,12 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  apps: [
+    {
+      name: "Cloud-X-Backend",
+      script: "npm start",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+    },
+  ],
 };
